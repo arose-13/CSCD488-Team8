854850de641c072a2fe006425e43d93161604b6a
grahamna4 <nate.a.graham@gmail.com>
2023-01-10 23:33:35 -0800

    Maven Project WAR file generation fix
    Was using java 8, when the Maven project generation was using java 18.

--- team8/src/main/java/budgetapp/DatabaseConnection/JavaDBConnect.java
+++ team8/src/main/java/budgetapp/DatabaseConnection/JavaDBConnect.java
@@ -3,29 +3,74 @@ package budgetapp.DatabaseConnection;
 import java.sql.*;
 
 public class JavaDBConnect {
-    //Sanity Check with Java -> mysql db connection
+    private static Connection connection;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/budgetappuser";
+    private static final String USERNAME = "sqluser";
+    private static final String PASSWORD = "Team8Password";
+
     public static void main(String[] args) {
-        String dbURL = "jdbc:mysql://localhost:3306/budgetappuser";
-        String username = "sqluser";
-        String password = "Team8Password";
+        connectToDB();
+        fetchAllUsers();
+        closeDBConnection();
+    }
+
+    private static void connectToDB() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(dbURL, username, password);
+            connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error connecting to DB: " + e.getMessage());
+        }
+    }
+
+    private static void fetchAllUsers() {
+        try {
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("select * from `appusertable`");
 
-            while (resultSet.next()) {
+            while (resultSet.next()) { //This needs to be changed to be useful to the front end
                 System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+resultSet.getString(3)+" | "+resultSet.getString(4));
             }
+        } catch (SQLException e) {
+            System.out.println("Error fetching all users: " + e.getMessage());
+        }
+    }
 
+    private static void closeDBConnection() {
+        try {
             connection.close();
-
-            // Output expected
-            // 1 | testUser | notHashedPW | testEmail@email.com
-            // 2 | testUser2 | notHashedPW2 | testEmail@email.com2
-
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (SQLException e) {
+            System.out.println("Error closing DB connection: " + e.getMessage());
         }
     }
 }
+    
+
+
+
+    // //Sanity Check with Java -> mysql db connection
+    // public static void main(String[] args) {
+    //     String dbURL = "jdbc:mysql://localhost:3306/budgetappuser";
+    //     String username = "sqluser";
+    //     String password = "Team8Password";
+    //     try {
+    //         Class.forName("com.mysql.cj.jdbc.Driver");
+    //         Connection connection = DriverManager.getConnection(dbURL, username, password);
+    //         Statement statement = connection.createStatement();
+    //         ResultSet resultSet = statement.executeQuery("select * from `appusertable`");
+
+    //         while (resultSet.next()) {
+    //             System.out.println(resultSet.getInt(1)+" | "+resultSet.getString(2)+" | "+resultSet.getString(3)+" | "+resultSet.getString(4));
+    //         }
+
+    //         connection.close();
+
+    //         // Output expected
+    //         // 1 | testUser | notHashedPW | testEmail@email.com
+    //         // 2 | testUser2 | notHashedPW2 | testEmail@email.com2
+
+    //     } catch (Exception e) {
+    //         System.out.println(e);
+    //     }
+    // }
+
