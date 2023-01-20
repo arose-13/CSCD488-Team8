7bdd38579c24d98ce95eb238a80cd4644c4d6819
grahamna4 <nate.a.graham@gmail.com>
2023-01-19 20:11:34 -0800

    there's a naming conflict that's not being updated

--- team8/src/main/java/budgetapp/resources/testResource.java
+++ team8/src/main/java/budgetapp/resources/testResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/test")
-public class TestResource {
+public class testResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
