20d154c8266b4a4e2c5038ed9b9acb58d5df3db5
grahamna4 <nate.a.graham@gmail.com>
2023-01-30 17:41:26 -0800

    the tomcat

---
+++ apache-tomcat-9.0.69/webapps/team8-0.1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home Page MAN</title>
+    <link rel="stylesheet" href="css/default.css" type="text/css">
+    <?php include 'components/head.html'; ?>
+</head>
+
+<body>
+
+    <?php include 'components/main-header.html'; ?>
+
+    <main>
+
+        <p>Welcome to the homepage, DUDE!</p>
+
+    </main>
+
+    <?php include 'components/main-footer.html'; ?>
+
+</body>
+
+</html>
