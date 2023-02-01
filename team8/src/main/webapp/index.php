2695785db96e1869617a686b2f5f627888645b82
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-01-31 21:47:38 -0800

    API Test added to `index.php`
    Shows how to get the API working.

--- team8/src/main/webapp/index.php
+++ team8/src/main/webapp/index.php
@@ -14,6 +14,27 @@
     <main>
 
         <p>Welcome to the homepage!</p>
+
+        <?php
+            // Initialize the curl resource
+            $resource = curl_init("localhost:8080/team8-0.1/webapi/test"); 
+            // Set the option ("setopt") for the curl resource to retrieve data.
+            // Otherwise, it will just return 'true' or 'false', depending on whether or not it was successful.
+            curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+            // Execute the request and store it in $result
+            $result = curl_exec($resource);
+            // Close the resource (you want to do this as it will free system resources)
+            curl_close($resource);
+
+            if ($result)
+            {
+                echo $result;
+            }
+            else
+            {
+                echo "Something didn't go quite right...";
+            }
+        ?>
         
     </main>
 
