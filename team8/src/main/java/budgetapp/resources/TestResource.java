46c5b77fd15c2da7ac92251d47211f4683e2aa7c
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-03-10 20:48:57 -0800

    Test Added

--- team8/src/main/java/budgetapp/resources/TestResource.java
+++ team8/src/main/java/budgetapp/resources/TestResource.java
@@ -1,6 +1,7 @@
 package budgetapp.resources;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -12,5 +13,14 @@ public class TestResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String testString() {
         return "Hello Worldz Test";
+    }
+
+    @POST
+    @Path("/return-test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String returnTest(String data) {
+        if (data != null)
+            return "You sent '" + data + "'";
+        return "'data' is null!";
     }
 }
--- team8/src/main/webapp/index.php
+++ team8/src/main/webapp/index.php
@@ -14,6 +14,27 @@
     <main>
 
         <p>Welcome to the homepage!</p>
+
+        <?php
+        $resource = curl_init("localhost:8080/team8-0.1/webapi/test/return-test"); 
+        curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($resource, CURLOPT_POST, 1);
+
+        $outBoundQuery = http_build_query(array("data" => "Hello World!"));
+        curl_setopt($resource, CURLOPT_POSTFIELDS, $outBoundQuery);
+
+        $result = curl_exec($resource);
+        curl_close($resource);
+
+        if ($result)
+        {
+            echo $result;
+        }
+        else
+        {
+            echo "'result' variable is null!";
+        }
+        ?>
         
     </main>
 
