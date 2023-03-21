19d1cf8d846f0551072beaa1a362ceffd6eef888
Helaman99 <80353517+arose-13@users.noreply.github.com>
2023-03-21 10:28:21 -0700

    Added correct header and footer to the change pages

--- team8/src/main/webapp/changeEmail.php
+++ team8/src/main/webapp/changeEmail.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-    <?php include 'components/main-header.html'; ?>
+    <?php include 'components/app-header.html'; ?>
 
     
     <div class="dashboard-grid" id="account-info">
@@ -49,4 +49,4 @@
 
 </body>
 
-<?php include 'components/main-footer.html'; ?>
+<?php include 'components/app-footer.html'; ?>
